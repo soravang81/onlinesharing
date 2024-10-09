705b70fda9751ee46683f3335394eb913406eb73
soravang81 <soravang81@gmail.com>
2024-10-09 18:39:30 +0530

    finish

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       backgroundColor="black"
       className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
     >
-      <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+      <h2 className="text-white text-5xl md:text-6xl font-bold text-center">
         Online Sharing made easy
       </h2>
       <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
