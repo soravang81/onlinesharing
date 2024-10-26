c62e6b2da6218a30e45c6627fb353ea7e68d6c7c
soravang81 <soravang81@gmail.com>
2024-10-26 10:54:22 +0530

    bg fx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 
 export default function Footer () {
-    return <div className="w-full xl:px-60 lg:px-40 md:px-20 sm:px-8 px-2 flex flex-col sm:flex-row items-center justify-between gap-2 absolute bottom-8 w-fulll bg-black text-white">
+    return <div className="w-full z-10 xl:px-60 lg:px-40 md:px-20 sm:px-8 px-2 flex flex-col sm:flex-row items-center justify-between gap-2 absolute bottom-8 w-fulll bg-black text-white">
         <section className="flex items-center gap-2">
             <p className="text-center text-white font-sans">© 2023 Online-Sharing.io</p>
         </section>
